fb9b155806363dd06b00879710f6bd0a1c152673
Nils Moehrle <nils@kuenstle-moehrle.de>
2024-08-09 23:57:01 +0200

    Add proper tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,22 @@
 #include <ujson.hpp>
 
 #include <iostream>
-#include <sstream>
 
 int main()
 {
-    std::stringstream ss;
-    ss << std::cin.rdbuf();
-    std::cout << ujson::serialize(ujson::parse(ss.str()));
+    auto obj = ujson::object();
+    obj["s"] = std::string("str");
+    obj["i"] = 1337;
+    obj["d"] = 3.14;
+    obj["t"] = true;
+    obj["f"] = false;
+    obj["0"] = nullptr;
+    obj["arr"] = ujson::array({42, 2.71, "foo", true, false, nullptr});
+    auto val = ujson::value(obj);
+    const auto str = ujson::serialize(val);
+    std::cout << str << std::endl;
+    if (val != ujson::parse(str))
+    {
+        exit(EXIT_FAILURE);
+    }
 }
